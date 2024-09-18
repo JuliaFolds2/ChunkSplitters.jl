e9eba840863d71420308dabe884773cdd21bf7c6
Carsten Bauer <crstnbr@gmail.com>
2024-09-18 23:49:28 +0200

    continue

--- src/ChunkSplitters.jl
+++ src/ChunkSplitters.jl
@@ -5,7 +5,7 @@ include("internals.jl")
 
 export chunk_indices, chunk, SplitStrategy, BatchSplit, ScatterSplit
 if VERSION >= v"1.11.0-DEV.469"
-    eval(Meta.parse("public is_chunkable, ChunksIterator, ChunksIterator"))
+    eval(Meta.parse("public is_chunkable"))
 end
 
 end # module
