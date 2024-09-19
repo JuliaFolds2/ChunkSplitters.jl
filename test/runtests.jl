74d5ed30427fe76e3288930627a6301be7b97dcb
Carsten Bauer <crstnbr@gmail.com>
2024-09-20 01:34:47 +0200

    test_broken -> test

--- test/runtests.jl
+++ test/runtests.jl
@@ -406,6 +406,6 @@ end
         b = @benchmark $f_chunk($x; n=4) samples = 1 evals = 1
         @test b.allocs == 0
         b = @benchmark $f_chunk($x; size=10) samples = 1 evals = 1
-        @test_broken b.allocs == 0
+        @test b.allocs == 0
     end
 end
